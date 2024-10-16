38f6ace46948966a8a4506fd09a7565012faa7cb
Florin.Popa <florin.popa@moduscreate.com>
2024-10-16 12:13:08 +0300

    fix exec

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const exec = require('@actions/core');
+const exec = require('@actions/exec');
 
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
